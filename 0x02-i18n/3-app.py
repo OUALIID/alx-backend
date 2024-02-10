7f534110d7fe7c9b75a04503981573d6ff2caa80
OUALIID <walidelhadim8@gmail.com>
2024-02-10 04:44:45 +0100

    Function _ to specify the parameters of my templates.

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" A basic Flask application with one path and an HTML template. """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from flask_babel import _
+
+
+class Config():
+    """ Configuration class for the Flask app. """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Determine the locale for this user."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ Returns Hello world """
+    return render_template(
+        '3-index.html',
+        title=_('home_title'),
+        header=_('home_header')
+        )
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000')
