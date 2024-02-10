28b95d5f7075a9e378ccf4a3345f92a34be9e416
OUALIID <walidelhadim8@gmail.com>
2024-02-10 05:01:03 +0100

    I made some modifications to the file

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,14 +2,24 @@
 """ A basic Flask application with one path and an HTML template. """
 from flask import Flask, render_template, request
 from flask_babel import Babel
+
+
+class Config(object):
+    """ Configuration class for the Flask app. """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
 babel = Babel(app)
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
     """ Determine the locale for this user."""
-    return request.accept_languages.best_match()
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
