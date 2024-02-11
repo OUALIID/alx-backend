38125f427b33a254e963c40a6e40c97add78b52c
OUALIID <walidelhadim8@gmail.com>
2024-02-11 02:53:39 +0100

    Update the code

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,40 +1,35 @@
 #!/usr/bin/env python3
-"""
-0x02. i18n
-"""
+""" A basic Flask application with one path and an HTML template. """
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config:
-    """The configuration class for the application."""
-
+class Config():
+    """ Configuration class for the Flask app. """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
+babel = Babel(app)
 app.config.from_object(Config)
 
 
 @app.route("/")
-def main():
-    """A basic flask method."""
-    return render_template("4-index.html")
+def index():
+    """ Returns Hello world """
+    return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """Determine which locale to use based on user's request header."""
-    language = request.args['locale']
-    if 'locale' in request.args and language in app.config['LANGUAGES']:
+    """ Determine the locale for this user."""
+    language = request.args.get('locale')
+    if language and language in app.config['LANGUAGES']:
         return language
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
