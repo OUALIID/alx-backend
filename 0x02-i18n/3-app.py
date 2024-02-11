88786551806de7aec9b3cbbf718a0a03969134bc
OUALIID <walidelhadim8@gmail.com>
2024-02-11 02:09:40 +0100

    Try to fix problems

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python3
-""" Route module for the API """
-from flask import Flask, render_template
+""" A basic Flask application with one path and an HTML template. """
+from flask import Flask, render_template, request
 from flask_babel import Babel
+
+
+class Config():
+    """ Configuration class for the Flask app. """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 babel = Babel(app)
-
-
-class Config(object):
-    """ Available languages class """
-    LANGUAGES = ['en', 'fr']
-    # these are the inherent defaults just btw
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-# set the above class object as the configuration for the app
 app.config.from_object('1-app.Config')
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route("/")
 def index() -> str:
-    """ GET /
-    Return:
-      - 1-index.html
-    """
-    return render_template('3-index.html')
+    """ Returns Hello world """
+    return render_template('1-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """ Determines best match for supported languages """
+    """ Determine the locale for this user."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
