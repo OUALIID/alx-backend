8e45b3792123dd7544ddafade14e6c6935f42878
OUALIID <walidelhadim8@gmail.com>
2024-02-12 23:27:14 +0100

    LFUCache class which inherits from BaseCaching and is a caching system.

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Module for implementing a LFUCache.
+"""
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ Class representing a LFUCache."""
+    def __init__(self):
+        """ Initialize a new LFUCache. """
+        super().__init__()
+        self.lfu_cache = []
+
+    def put(self, key, item):
+        """ Add an item to the cache. """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.lfu_cache.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.lfu_cache.pop(0)
+                del self.cache_data[lru_key]
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.lfu_cache.append(key)
+        else:
+            return None
+
+    def get(self, key):
+        """ Retrieve an item from the cache. """
+        if key in self.cache_data:
+            self.lfu_cache.remove(key)
+            self.lfu_cache.append(key)
+            return self.cache_data[key]
+        return None
