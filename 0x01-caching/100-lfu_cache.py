dc40f4e42b8cc67aa631e576e3b1132b12f0402d
OUALIID <walidelhadim8@gmail.com>
2024-02-13 00:20:59 +0100

    Return to the first version of code

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,23 +10,24 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """ Initialize a new LFUCache. """
         super().__init__()
-        self.key_usage_count = {}
+        self.lfu_cache = []
 
     def put(self, key, item):
         """ Add an item to the cache. """
         if key is not None and item is not None:
-            self.key_usage_count[key] = self.key_usage_count.get(key, 0) + 1
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                lfu_key = min(self.key_usage_count, key=self.key_usage_count.get)
-                if lfu_key in self.cache_data:
-                    del self.cache_data[lfu_key]
-                    del self.key_usage_count[lfu_key]
-                    print("DISCARD:", lfu_key)
+            if key in self.cache_data:
+                self.lfu_cache.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.lfu_cache.pop(0)
+                del self.cache_data[lru_key]
+                print("DISCARD:", lru_key)
             self.cache_data[key] = item
+            self.lfu_cache.append(key)
+        else:
+            return None
 
     def get(self, key):
         """ Retrieve an item from the cache. """
         if key in self.cache_data:
-            self.key_usage_count[key] += 1
             return self.cache_data[key]
         return None
