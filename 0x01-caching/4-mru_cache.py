bc13288d82585f3f3b09210bf18ee9741daf7c4c
OUALIID <walidelhadim8@gmail.com>
2024-02-12 23:15:38 +0100

    LRUCache class which inherits from BaseCaching and is a caching system.

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python
+"""
+Module for implementing a MRUCache.
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ Class representing a MRUCache."""
+    def __init__(self):
+        """ Initialize a new MRUCache. """
+        super().__init__()
+        self.mru_cache = []
+
+    def put(self, key, item):
+        """ Add an item to the cache. """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.mru_cache.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.mru_cache.pop()
+                del self.cache_data[lru_key]
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.mru_cache.append(key)
+        else:
+            return None
+
+    def get(self, key):
+        """ Retrieve an item from the cache. """
+        if key in self.cache_data:
+            # self.mru_cache.remove(key)
+            self.mru_cache.append(key)
+            return self.cache_data[key]
+        return None
